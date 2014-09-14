06511dac932f6ebdb8041338ddf6b43490fa6b06
Olivier Laviale <contact@weirdog.com>
2014-09-14 17:23:42 +0200

    Added the ToSlug interface

---
+++ lib/ToSlug.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\Routing;
+
+/**
+ * An insterface used to turn an instance into a slug.
+ *
+ * @see http://en.wikipedia.org/wiki/Semantic_URL#Slug
+ */
+interface ToSlug
+{
+	/**
+	 * Return a slug representation of the instance.
+	 *
+	 * @return string
+	 */
+	public function to_slug();
+}
