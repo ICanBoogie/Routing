89c029fd6bef51d00f7f48d36cff746877bfb1e1
Olivier Laviale <olivier.laviale@gmail.com>
2015-05-09 10:14:21 +0200

    Forgot some files there

---
+++ tests/ControllerTest/MySampleController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\Routing\ControllerTest;
+
+use ICanBoogie\HTTP\Request;
+use ICanBoogie\Routing\Controller;
+
+class MySampleController extends Controller
+{
+	protected function action(Request $request)
+	{
+		$request->test->assertInstanceOf('ICanBoogie\HTTP\Request', $request);
+		$request->test->assertEquals(1, func_num_args());
+		$request->test->assertEquals("my_sample", $this->name);
+		$request->test->assertInstanceOf('ICanBoogie\Routing\Route', $this->route);
+
+		return 'HERE';
+	}
+}
