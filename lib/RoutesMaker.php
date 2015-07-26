9992378f1dc3f318bee0b5a2ba1346f415e4859a
Olivier Laviale <olivier.laviale@gmail.com>
2015-07-26 22:53:03 +0200

    Added RoutesMaker::actions()

--- lib/RoutesMaker.php
+++ lib/RoutesMaker.php
@@ -19,6 +19,46 @@ use ICanBoogie\HTTP\Request;
 class RoutesMaker
 {
 	/**
+	 * @param string $name
+	 * @param string $controller
+	 * @param array $actions Action templates.
+	 * @param array $options The following options are available:
+	 *
+	 * - `only`: Only the routes specified are made.
+	 * - `except`: The routes specified are excluded.
+	 * - `id_name`: Name of the identifier property. Defaults to `id`.
+	 * - `id_regex`: Regex of the identifier value. Defaults to `\d+`.
+	 * - `as`: Specifies the `as` option of the routes created.
+	 *
+	 * @return array
+	 */
+	static public function actions($name, $controller, $actions, array $options = [])
+	{
+		$options = self::normalize_options($options);
+		$actions = static::filter_actions($actions, $options);
+		$actions = static::resolve_patterns($name, $actions, $options);
+
+		$options_as = $options['as'];
+		$routes = [];
+
+		foreach ($actions as $action => list($pattern, $via))
+		{
+			$as = empty($options_as[$action]) ? "{$name}:{$action}" : $options_as[$action];
+
+			$routes[$as] = [
+
+				'pattern' => $pattern,
+				'controller' => "{$controller}#{$action}",
+				'via' => $via,
+				'as' => $as
+
+			];
+		}
+
+		return $routes;
+	}
+
+	/**
 	 * Makes route definitions for a resource.
 	 *
 	 * @param string $name
@@ -30,33 +70,69 @@ class RoutesMaker
 	 * - `id_name`: Name of the identifier property. Defaults to `id`.
 	 * - `id_regex`: Regex of the identifier value. Defaults to `\d+`.
 	 * - `as`: Specifies the `as` option of the routes created.
+	 * - `actions`: Additional actions templates.
 	 *
 	 * @return array
 	 */
 	static public function resource($name, $controller, array $options = [])
 	{
-		$actions = [
+		$options = static::normalize_options($options);
+		$actions = array_merge(static::get_resource_actions(), $options['actions']);
 
-			'index'   => [ '/{resource}',           Request::METHOD_GET ],
-			'create'  => [ '/{resource}/create',    Request::METHOD_GET ],
-			'store'   => [ '/{resource}',           Request::METHOD_POST ],
-			'show'    => [ '/{resource}/{id}',      Request::METHOD_GET ],
-			'edit'    => [ '/{resource}/{id}/edit', Request::METHOD_GET ],
-			'update'  => [ '/{resource}/{id}',      [ Request::METHOD_PUT, Request::METHOD_PATCH ] ],
-			'destroy' => [ '/{resource}/{id}',      Request::METHOD_DELETE ]
+		return static::actions($name, $controller, $actions, $options);
+	}
 
-		];
-
-		$options += [
+	/**
+	 * Normalizes options.
+	 *
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	static protected function normalize_options(array $options)
+	{
+		return $options + [
 
 			'id_name' => 'id',
 			'id_regex' => '\d+',
-			'only' => [],
-			'except' => [],
-			'as' => []
+			'only' => [ ],
+			'except' => [ ],
+			'as' => [ ],
+			'actions' => [ ]
 
 		];
+	}
 
+	/**
+	 * Returns default resource actions.
+	 *
+	 * @return array
+	 */
+	static protected function get_resource_actions()
+	{
+		return [
+
+			'index'   => [ '/{name}',           Request::METHOD_GET ],
+			'create'  => [ '/{name}/create',    Request::METHOD_GET ],
+			'store'   => [ '/{name}',           Request::METHOD_POST ],
+			'show'    => [ '/{name}/{id}',      Request::METHOD_GET ],
+			'edit'    => [ '/{name}/{id}/edit', Request::METHOD_GET ],
+			'update'  => [ '/{name}/{id}',      [ Request::METHOD_PUT, Request::METHOD_PATCH ] ],
+			'destroy' => [ '/{name}/{id}',      Request::METHOD_DELETE ]
+
+		];
+	}
+
+	/**
+	 * Filters actions according to only/except options.
+	 *
+	 * @param array $actions
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	static protected function filter_actions(array $actions, array $options = [])
+	{
 		if ($options['only'])
 		{
 			$actions = array_intersect_key($actions, array_flip((array) $options['only']));
@@ -67,25 +143,28 @@ class RoutesMaker
 			$actions = array_diff_key($actions, array_flip((array) $options['except']));
 		}
 
+		return $actions;
+	}
+
+	/**
+	 * Replaces pattern placeholders.
+	 *
+	 * @param string $name
+	 * @param array $actions
+	 * @param array $options
+	 *
+	 * @return array
+	 */
+	static protected function resolve_patterns($name, array $actions, $options)
+	{
 		$id = "<{$options['id_name']}:{$options['id_regex']}>";
-		$replace = [ '{resource}' => $name, '{id}' => $id ];
-		$options_as = $options['as'];
-		$routes = [];
+		$replace = [ '{name}' => $name, '{id}' => $id ];
 
-		foreach ($actions as $action => list($pattern, $via))
+		foreach ($actions as $action => &$template)
 		{
-			$as = empty($options_as[$action]) ? "{$name}:{$action}" : $options_as[$action];
-
-			$routes[$as] = [
-
-				'pattern' => strtr($pattern, $replace),
-				'controller' => "{$controller}#{$action}",
-				'via' => $via,
-				'as' => $as
-
-			];
+			$template[0] = strtr($template[0], $replace);
 		}
 
-		return $routes;
+		return $actions;
 	}
 }
