d5478268ca445fdfe5b8b256b1ee7ece80f3e518
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-22 01:30:39 +0200

    Add Memoize

---
+++ lib/RouteProvider/Memoize.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\Routing\RouteProvider;
+
+use ICanBoogie\Routing\IterableRouteProvider;
+use ICanBoogie\Routing\Route;
+use Traversable;
+
+/**
+ * Speed up route resolution for predicate {@link ById}, {@link ByAction}, and {@link ByUri}.
+ */
+final class Memoize implements IterableRouteProvider
+{
+    public function __construct(
+        private readonly IterableRouteProvider $inner_provider
+    ) {
+    }
+
+    public function getIterator(): Traversable
+    {
+        return $this->inner_provider->getIterator();
+    }
+
+    private IterableRouteProvider $by_id;
+    private IterableRouteProvider $by_action;
+    private IterableRouteProvider $by_uri;
+
+    public function route_for_predicate(callable $predicate): ?Route
+    {
+        if ($predicate instanceof ById) {
+            return ($this->by_id ??= new MemoizeById($this->inner_provider))->route_for_predicate($predicate);
+        }
+
+        if ($predicate instanceof ByAction) {
+            return ($this->by_action ??= new MemoizeByAction($this->inner_provider))->route_for_predicate($predicate);
+        }
+
+        if ($predicate instanceof ByUri) {
+            return ($this->by_uri ??= new MemoizeByUri($this->inner_provider))->route_for_predicate($predicate);
+        }
+
+        return $this->inner_provider->route_for_predicate($predicate);
+    }
+}
---
+++ tests/RouteProvider/MemoizeTest.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Test\ICanBoogie\Routing\RouteProvider;
+
+use ICanBoogie\Routing\IterableRouteProvider;
+use ICanBoogie\Routing\Route;
+use ICanBoogie\Routing\RouteProvider\ByAction;
+use ICanBoogie\Routing\RouteProvider\ById;
+use ICanBoogie\Routing\RouteProvider\ByUri;
+use ICanBoogie\Routing\RouteProvider\Immutable;
+use ICanBoogie\Routing\RouteProvider\Memoize;
+use PHPUnit\Framework\TestCase;
+
+use function implode;
+
+final class MemoizeTest extends TestCase
+{
+    private IterableRouteProvider $provider;
+    private SpyIterableRouteProvider $spy;
+    private Route $r1;
+    private Route $r2;
+    private Route $r3;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->provider = new Memoize(
+            $this->spy = new SpyIterableRouteProvider(
+                new Immutable([
+                    $this->r1 = new Route('/', 'page:home'),
+                    $this->r2 = new Route('/about.html', 'page:about', id: 'about'),
+                    $this->r3 = new Route('/contact.html', 'page:contact'),
+                ])
+            )
+        );
+    }
+
+    public function test_iterator(): void
+    {
+        $actions = [];
+
+        foreach ($this->provider as $route) {
+            $actions[] = $route->action;
+        }
+
+        $this->assertSame("page:home page:about page:contact", implode(' ', $actions));
+        $this->assertEquals(0, $this->spy->times_route_for_predicate);
+        $this->assertEquals(1, $this->spy->times_iterator);
+    }
+
+    public function test_other_predicates_are_forwarded(): void
+    {
+        $this->assertSame(
+            $this->r1,
+            $this->provider->route_for_predicate(fn(Route $route): bool => $route === $this->r1)
+        );
+        $this->assertSame(
+            $this->r2,
+            $this->provider->route_for_predicate(fn(Route $route): bool => $route === $this->r2)
+        );
+        $this->assertSame(
+            $this->r3,
+            $this->provider->route_for_predicate(fn(Route $route): bool => $route === $this->r3)
+        );
+        $this->assertEquals(3, $this->spy->times_route_for_predicate);
+        $this->assertEquals(0, $this->spy->times_iterator);
+    }
+
+    public function test_by_id(): void
+    {
+        $this->assertNull($this->provider->route_for_predicate(new ById('page:home')));
+        $this->assertSame($this->r2, $this->provider->route_for_predicate(new ById('about')));
+        $this->assertNull($this->provider->route_for_predicate(new ById('page:contact')));
+
+        $this->assertEquals(0, $this->spy->times_route_for_predicate);
+        $this->assertEquals(1, $this->spy->times_iterator);
+    }
+
+    public function test_by_action(): void
+    {
+        $this->assertSame($this->r1, $this->provider->route_for_predicate(new ByAction('page:home')));
+        $this->assertSame($this->r2, $this->provider->route_for_predicate(new ByAction('page:about')));
+        $this->assertSame($this->r3, $this->provider->route_for_predicate(new ByAction('page:contact')));
+
+        $this->assertEquals(0, $this->spy->times_route_for_predicate);
+        $this->assertEquals(1, $this->spy->times_iterator);
+    }
+
+    public function test_by_uri(): void
+    {
+        $this->assertSame($this->r1, $this->provider->route_for_predicate(new ByUri('/')));
+        $this->assertSame($this->r2, $this->provider->route_for_predicate(new ByUri('/about.html')));
+        $this->assertSame($this->r3, $this->provider->route_for_predicate(new ByUri('/contact.html')));
+
+        $this->assertEquals(0, $this->spy->times_route_for_predicate);
+        $this->assertEquals(1, $this->spy->times_iterator);
+    }
+}
