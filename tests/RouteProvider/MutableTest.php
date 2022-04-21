3701b50339375aaff1600d1ec4397e5ab11b633d
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-22 01:49:04 +0200

    Test serialization

--- tests/RouteProvider/MutableTest.php
+++ tests/RouteProvider/MutableTest.php
@@ -11,35 +11,56 @@
 
 namespace Test\ICanBoogie\Routing\RouteProvider;
 
+use ICanBoogie\Routing\IterableRouteProvider;
 use ICanBoogie\Routing\Route;
 use ICanBoogie\Routing\RouteProvider\ByAction;
 use ICanBoogie\Routing\RouteProvider\Mutable;
 use PHPUnit\Framework\TestCase;
 
 use function implode;
+use function serialize;
+use function unserialize;
 
 final class MutableTest extends TestCase
 {
+    private IterableRouteProvider $provider;
+    private Route $r1;
+    private Route $r2;
+    private Route $r3;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->provider = new Mutable();
+        $this->provider->add_routes(
+            $this->r1 = new Route('/', 'page:home'),
+            $this->r2 = new Route('/about.html', 'page:about'),
+            $this->r3 = new Route('/contact.html', 'page:contact'),
+        );
+    }
+
     public function test_routes(): void
     {
-        $routes = new Mutable();
-        $routes->add_routes(
-            $r1 = new Route('/', 'page:home'),
-            $r2 = new Route('/about.html', 'page:about'),
-            $r3 = new Route('/contact.html', 'page:contact'),
-        );
-
-        $this->assertSame($r1, $routes->route_for_predicate(new ByAction('page:home')));
-        $this->assertSame($r2, $routes->route_for_predicate(new ByAction('page:about')));
-        $this->assertSame($r3, $routes->route_for_predicate(new ByAction('page:contact')));
-        $this->assertNull($routes->route_for_predicate(new ByAction('madonna')));
+        $this->assertSame($this->r1, $this->provider->route_for_predicate(new ByAction('page:home')));
+        $this->assertSame($this->r2, $this->provider->route_for_predicate(new ByAction('page:about')));
+        $this->assertSame($this->r3, $this->provider->route_for_predicate(new ByAction('page:contact')));
+        $this->assertNull($this->provider->route_for_predicate(new ByAction('madonna')));
 
         $actions = [];
 
-        foreach ($routes as $route) {
+        foreach ($this->provider as $route) {
             $actions[] = $route->action;
         }
 
         $this->assertSame("page:home page:about page:contact", implode(' ', $actions));
+    }
+
+    public function test_serialize_unserialize(): void
+    {
+        $serialized = serialize($this->provider);
+        $provider = unserialize($serialized);
+
+        $this->assertEquals($this->provider, $provider);
     }
 }
